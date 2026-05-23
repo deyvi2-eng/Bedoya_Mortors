6397f501c16845e9a1ac98a37a507efb81689be4
Deyvi10 <139245885+Deyvi10@users.noreply.github.com>
2026-05-23 15:06:13 -0500

    Modificación requerimientos

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 #DEBUG = env('DEBUG')
 DEBUG = True 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.onrender.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
